4c13fd3782f687be9600d30c18dda2dbc46f5adf
s-joi <s-joi@53S500042581.it.bsd405.org>
2019-09-20 08:08:33 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -17,11 +17,70 @@
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void main (String[] args) {
+		bubbles1();
+		fishes1();
+		bubbles2();
+		fishes2();
+		bubbles3();
+		fishes3();
+		bubbles4();
+		fishes4();
+		projectCaption();
+		
+	
 	}
+	public static void bubbles1() {
+		 System.out.println("\n\n\t\t\tO");
+		 System.out.println("\n\n\t\t    o");
+	 
+	}
+	public static void bubbles2() {
+		 System.out.println("\t\t\tO");
+		 System.out.println("\n\n\t\t\t\t    o"); 
+	}
+	public static void bubbles3() {
+		 System.out.println("\t\t\tO");
+		 System.out.println("\n\n\t\t\t    o"); 
+	}
+	public static void bubbles4() {
+		 System.out.println("\t\tO");
+		 System.out.println("\n\n\t    o"); 
+	}
+	 
+	public static void fishes1() {
+		System.out.print("\t><\\\\\\^>");
+	}
+	public static void fishes2() {
+		System.out.println("\t\t\t\t\t<^)))><");
+		seaweed();
+		seaweed2();
+	}
+	public static void fishes3() {
+		System.out.println("\t\t\t\t<^///><");
+	}
+	public static void fishes4() {
+		System.out.println("><||||^>");
+		System.out.print("\t\t\t\t\t");
+		seaweed();
+	
+		bubbles1();
+		fishes1();
+	
+	}
+	public static void seaweed() {
+		System.out.print(" \\\\//");
+	
+	}
+	public static void seaweed2() {
+		System.out.print("\n\\\\\\///");
+	}
+	public static void projectCaption() {
+		System.out.println("\n\n\n\n\nCaption:");
+		System.out.println("\n\tA group of fishes, bubbles, and seaweed");
+	
+	}
+	
+	
+	
 }
